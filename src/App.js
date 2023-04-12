3415bdeea04d7e3f50962eaec74b3da964c30afa
MidiComputing <bennasseur.medmahdi@gmail.com>
2023-04-12 20:59:11 +0100

    new update 0.5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI</p>
+        <p>Successfully Deployed <code>A React application</code> to Netlify From Hatem</p>
 
         <a
           className="App-link"
