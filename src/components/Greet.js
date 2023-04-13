5617e8d8a352c43de5f49a5f24913f64363c02e9
MidiComputing <bennasseur.medmahdi@gmail.com>
2023-04-13 18:09:07 +0100

    update 0.6

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/* function Greet (){
+    return <h1>Hello Midi</h1>
+} */
+
+export const Greet = () => {
+    return <h1>Hello Arrow Function</h1>
+}
+
+//export default Greet
