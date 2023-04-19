f46879a7c2a264fc3be4f726930287257a9a23fb
MidiComputing <bennasseur.medmahdi@gmail.com>
2023-04-19 18:47:49 +0100

    update 0.8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {Greet} from './components/Greet';
-import Welcome from './components/Welcome';
+import {Greet} from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
